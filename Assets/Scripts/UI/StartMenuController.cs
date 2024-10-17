b40a70fe492f79e0b206caccfc8ae65c90f5d806
afiseleo <123489414+fiseleo@users.noreply.github.com>
2024-10-18 04:56:00 +0800

    add StartMenu
    add StartMenu

---
+++ Assets/Scripts/UI/StartMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // 加载场景，确保场景名正确
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        // 退出游戏
+        Application.Quit();
+    }
+}
